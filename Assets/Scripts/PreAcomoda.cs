72fbece0d5d4ad09b26d83517d08ee1d2a429029
Daniel Yamamoto <79288816+daniel-ensamble@users.noreply.github.com>
2021-10-13 11:04:28 -0500

    Refactor buttons scenes

--- Assets/Scripts/PreAcomoda.cs
+++ Assets/Scripts/PreAcomoda.cs
@@ -1,16 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PreAcomoda : MonoBehaviour
-{
-    public void GoTo()
-    {
+public class PreAcomoda : MonoBehaviour {
+    public void GoTo() {
         ChangeScene.Change("Acomoda");
     }
 
-    public void ReturnTo()
-    {
+    public void ReturnTo() {
         ChangeScene.Change("CasaDeCampoSc");
     }
 }
