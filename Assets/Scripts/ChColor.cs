26317f5a5bfd20f6b94a0bef75542e39a41d82e0
Daniel Yamamoto <79288816+daniel-ensamble@users.noreply.github.com>
2021-10-14 18:38:11 -0500

    Color script updated

--- Assets/Scripts/ChColor.cs
+++ Assets/Scripts/ChColor.cs
@@ -2,167 +2,26 @@ using UnityEngine;
 
 public class ChColor : MonoBehaviour {
     public GameObject jugador;
-    Material[] mats;
     
-    public void ChangeColorRed() {
-        LoadManager.player.color = "red";
-        mats = jugador.transform.GetChild(0).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("red") as Texture;
-        mats = jugador.transform.GetChild(2).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("red") as Texture;
-        
-        for (int i = 2; i <= 10; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("red") as Texture;
-        }
-        
-        for (int i = 12; i <= 25; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("red") as Texture;
-        }
-    }
+    [SerializeField] private string color = "red";
 
-    public void ChangeColorBlue() {
-        LoadManager.player.color = "blue";
-        mats = jugador.transform.GetChild(0).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("blue") as Texture;
-        mats = jugador.transform.GetChild(2).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("blue") as Texture;
-        
-        for (int i = 2; i <= 10; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("blue") as Texture;
-        }
-        
-        for (int i = 12; i <= 25; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("blue") as Texture;
-        }
-    }
+    private Material[] mats;
     
-    public void ChangeColorOrange() {
-        LoadManager.player.color = "orange";
+    public void ChangeColor(string color) {
+        LoadManager.player.color = color;
         mats = jugador.transform.GetChild(0).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("orange") as Texture;
+        mats[0].mainTexture = Resources.Load(color) as Texture;
         mats = jugador.transform.GetChild(2).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("orange") as Texture;
+        mats[0].mainTexture = Resources.Load(color) as Texture;
         
         for (int i = 2; i <= 10; i++) {
             mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("orange") as Texture;
+            mats[0].mainTexture = Resources.Load(color) as Texture;
         }
         
         for (int i = 12; i <= 25; i++) {
             mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("orange") as Texture;
-        }
-    }
-
-    public void ChangeColorYellow() {
-        LoadManager.player.color = "yellow";
-        mats = jugador.transform.GetChild(0).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("yellow") as Texture;
-        mats = jugador.transform.GetChild(2).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("yellow") as Texture;
-        
-        for (int i = 2; i <= 10; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("yellow") as Texture;
-        }
-        
-        for (int i = 12; i <= 25; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("yellow") as Texture;
-        }
-    }
-
-    public void ChangeColorGreen() {
-        LoadManager.player.color = "green";
-        mats = jugador.transform.GetChild(0).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("green") as Texture;
-        mats = jugador.transform.GetChild(2).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("green") as Texture;
-        
-        for (int i = 2; i <= 10; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("green") as Texture;
-        }
-        
-        for (int i = 12; i <= 25; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("green") as Texture;
-        }
-    }
-
-    public void ChangeColorPurple() {
-        LoadManager.player.color = "purple";
-        mats = jugador.transform.GetChild(0).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("purple") as Texture;
-        mats = jugador.transform.GetChild(2).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("purple") as Texture;
-        
-        for (int i = 2; i <= 10; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("purple") as Texture;
-        }
-        
-        for (int i = 12; i <= 25; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("purple") as Texture;
-        }
-    }
-    
-    public void ChangeColorPink() {
-        LoadManager.player.color = "pink";
-        mats = jugador.transform.GetChild(0).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("pink") as Texture;
-        mats = jugador.transform.GetChild(2).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("pink") as Texture;
-        
-        for (int i = 2; i <= 10; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("pink") as Texture;
-        }
-        
-        for (int i = 12; i <= 25; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("pink") as Texture;
-        }
-    }
-
-    public void ChangeColorGray() {
-        LoadManager.player.color = "gray";
-        mats = jugador.transform.GetChild(0).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("gray") as Texture;
-        mats = jugador.transform.GetChild(2).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("gray") as Texture;
-        
-        for (int i = 2; i <= 10; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("gray") as Texture;
-        }
-        
-        for (int i = 12; i <= 25; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("gray") as Texture;
-        }
-    }
-
-    public void ChangeColorTeal() {
-        LoadManager.player.color = "teal";
-        mats = jugador.transform.GetChild(0).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("teal") as Texture;
-        mats = jugador.transform.GetChild(2).GetComponent<Renderer>().materials;
-        mats[0].mainTexture = Resources.Load("teal") as Texture;
-        
-        for (int i = 2; i <= 10; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("teal") as Texture;
-        }
-        
-        for (int i = 12; i <= 25; i++) {
-            mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
-            mats[0].mainTexture = Resources.Load("teal") as Texture;
+            mats[0].mainTexture = Resources.Load(color) as Texture;
         }
     }
 }
