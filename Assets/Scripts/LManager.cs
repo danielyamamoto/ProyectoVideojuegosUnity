c71ca4f3a8274e458917048331bacfd2df749e05
Daniel Yamamoto <79288816+daniel-ensamble@users.noreply.github.com>
2021-10-14 19:06:05 -0500

    Audio scene & collisions updated

--- Assets/Scripts/LManager.cs
+++ Assets/Scripts/LManager.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class LManager : MonoBehaviour {
     public GameObject jugador;
-    Material[] mats;
+    
+    private Material[] mats;
 
-    void Start() {
+    void Awake() {
         mats = jugador.transform.GetChild(0).GetComponent<Renderer>().materials;
-                mats[0].mainTexture = Resources.Load(LoadManager.player.color) as Texture;
-                mats = jugador.transform.GetChild(2).GetComponent<Renderer>().materials;
-                mats[0].mainTexture = Resources.Load(LoadManager.player.color) as Texture;
+        mats[0].mainTexture = Resources.Load(LoadManager.player.color) as Texture;
+        mats = jugador.transform.GetChild(2).GetComponent<Renderer>().materials;
+        mats[0].mainTexture = Resources.Load(LoadManager.player.color) as Texture;
                 
         for (int i = 2; i <= 10; i++) {
             mats = jugador.transform.GetChild(i).GetComponent<Renderer>().materials;
